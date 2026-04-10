b7156b3bcf901808b0bf59ee8f12bf1782cefac0
mingsit <mingsituk@gmail.com>
2026-04-10 18:18:19 +0100

    Upgrade maze, delete asset as it's not needed anymore.

--- main.py
+++ main.py
@@ -3,60 +3,60 @@ import random
 from copy import deepcopy
 import os
 
-Pixel = 20  # pixel for each cell
+Cell_pixel = 30  # pixel per each cell
 Choices = [(-1, 0), (1, 0), (0, -1), (0, 1)]  # left, right, up, down
 
 class MazeGenerateor():
     ''' Usage
-    1. call MazeGenerateor()
-    2. use .generate(size) function to generate the maze
-    3. use .output() to output the maze as image
-    4. (Optional) use .show_path() to output the path
+    call MazeGenerateor().generate(horizontal_size, vertical_size) to generate the maze as image
     '''
 
-    def __init__(self, path_length=3, fake_path_complexity=0.3):
-        self.path_length = path_length  # lenght of the correct path, ratio to size : 2 to 5
-        self.fake_path_complexity = fake_path_complexity
-        # 0.2 to 0.3, higher value means more fake paths, but with higher chance the maze can't be generated
-        image_list = ['black.png', 'white.png', 'green.png']
-        corner_image_list = ['top-left.png', 'bottom-left.png', 'bottom-right.png', 'top-right.png']
-        # 0 is black (wall), 1 is white (route)
-        self.images = [Image.open(os.path.join('asset', i)) for i in image_list]
-        self.corner_images = [Image.open(os.path.join('asset', i)).convert('RGBA') for i in corner_image_list]
-
     @staticmethod
-    def is_contradict(delta, target):
+    def is_undo(delta, target):
         return delta[0] + target[0] == 0 and delta[1] + target[1] == 0
 
-    @staticmethod
-    def has_overlap(path, target):
-        for cells in path[:-1]:
-            if (cells[0] + 1 == target[0] or cells[0] - 1 == target[0]) and (cells[1] == target[1]):
-                return True
-            elif (cells[1] + 1 == target[1] or cells[1] - 1 == target[1]) and (cells[0] == target[0]):
-                return True
+    def check_goal(self, target):
+        if target[0] <= -1 or target[0] >= self.size_x:
+            return True
+        if target[1] <= -1 or target[1] >= self.size_y:
+            return True
         return False
     
-    def check_goal(self, target):
-        for i in range(2):
-            if target[i] == 0 or target[i] == self.size - 1:
-                return True
-        return False
+    def change_walls(self, path):
+        # Change value of horizontal / vertical walls by inputted path
+        for step in range(len(path)-1):  # Don't loop last
+            try:
+                self.empty_cell.remove(path[step])  # can't remove first one
+            except KeyError:
+                pass
+            horizontal_action = path[step + 1][0] - path[step][0]
+            vertical_action = path[step + 1][1] - path[step][1]
+            if horizontal_action == 1:  # right
+                self.vertical_walls[path[step][1]][path[step][0]+1] = 1  # [which_column][which_row]
+            elif horizontal_action == -1:  # left
+                self.vertical_walls[path[step][1]][path[step][0]] = 1
+            elif vertical_action == 1:  # down
+                self.horizontal_walls[path[step][1]+1][path[step][0]] = 1
+            elif vertical_action == -1:  # up
+                self.horizontal_walls[path[step][1]][path[step][0]] = 1
+
 
     def create_path(self):
-        # Randomly generate a valid path from start to finish       
-        
+        # Randomly generate a valid path from start to finish
+        # Return Boolean (True if successfully generated path, False if not)
+
         path = []  # (x, y)
         mode = random.choice(['top', 'left'])
-        start_pos = random.randint(1, self.size)  # Don't start at the corner
         if mode == 'top':
+            start_pos = random.randint(0, self.size_x)
+            path.append((start_pos, -1))
             path.append((start_pos, 0))
-            path.append((start_pos, 1))
-            last_move = (0, 1)  # down
+            last_move = (0, 1)  # move down
         if mode == 'left':
+            start_pos = random.randint(0, self.size_y)
+            path.append((-1, start_pos))
             path.append((0, start_pos))
-            path.append((1, start_pos))
-            last_move = (1, 0)  # right
+            last_move = (1, 0)  # move right
 
         step = 1
         goal = False
@@ -66,136 +66,176 @@ class MazeGenerateor():
             all_choices = deepcopy(Choices)
             while not can_move:
                 if not all_choices:
-                    # No more move
-                    break
+                    # Cannot generate path for this random set
+                    return False
                 delta = random.choice(all_choices)
                 all_choices.remove(delta)
-                if MazeGenerateor.is_contradict(delta, last_move):
-                    # contradict with last delta
+                if MazeGenerateor.is_undo(delta, last_move):
+                    # ensure it doesn't undo the last move
                     continue
                 current_cell = path[-1]
                 target_cell = (current_cell[0] + delta[0], current_cell[1] + delta[1])
-                goal = self.check_goal(target_cell)
-                if goal and step < self.size * self.path_length:
-                    # Too short for a path
-                    goal = False
+                if target_cell in path:
+                    # Already walked into the cell before
                     continue
-                can_move = not MazeGenerateor.has_overlap(path, target_cell)
-                if can_move:
-                    # Update value for another loop
-                    step += 1
-                    last_move = delta
+                goal = self.check_goal(target_cell)
+                if goal:
+                    if step < (self.size_x+self.size_y)/2 * self.path_length:
+                        # Too short for a path
+                        goal = False
+                        continue
                     path.append(target_cell)
-            if not all_choices:
-                # Cannot generate path for this random set
-                return False
+                    break
+                # Update value for another loop
+                step += 1
+                last_move = delta
+                path.append(target_cell)
+                can_move = True
         
-        for cells in path:
-            self.matrix[cells[1]][cells[0]] = 1
+        # Change value of horizontal and vertical walls
+        self.change_walls(path)
         self.path = deepcopy(path)
         return True
 
-    def generate_fake_path(self):
-        # Randomly generate fake paths splitting from the correct path
+    def create_fake_path(self):
+        # Randomly generate fake paths, end if it overlaps any current path.
 
-        found_fake_start = False
-        fake_path = []
-        # Pick a random start point for fake path
-        while not found_fake_start:
-            random_pick = (random.randint(1, self.size-2), random.randint(1, self.size-2))
-            if not MazeGenerateor.has_overlap(self.path, random_pick) and not MazeGenerateor.has_overlap(self.fake_path, random_pick):
-                found_fake_start = True
-                fake_path.append(random_pick)
-        # Generate random route from fake start to a path (either valid or fake)
-        connected_to_any_path = False
+        path = []  # (x, y)
+        path.append(random.choice(list(self.empty_cell)))
         last_move = (0, 0)
-        while not connected_to_any_path:
-            can_connect = False
+
+        goal = False
+        while not goal:
+            # Check whether path can go to target
+            can_move = False
             all_choices = deepcopy(Choices)
-            while not can_connect:
+            while not can_move:
                 if not all_choices:
-                    # Cannot generate fake path for this random set
-                    return
+                    # Cannot generate path for this random set
+                    return False
                 delta = random.choice(all_choices)
                 all_choices.remove(delta)
-                if MazeGenerateor.is_contradict(delta, last_move):
-                    # contradict with last delta
+                if MazeGenerateor.is_undo(delta, last_move):
+                    # ensure it doesn't undo the last move
                     continue
-                current_cell = fake_path[-1]
+                current_cell = path[-1]
                 target_cell = (current_cell[0] + delta[0], current_cell[1] + delta[1])
-                if self.check_goal(target_cell):
-                    # fake path cannot escape the maze
-                    fake_goal = True
+                if target_cell in path:
+                    # Already walked into the cell before
                     continue
-                fake_goal = False
-                can_connect = not MazeGenerateor.has_overlap(fake_path, target_cell)
-                if can_connect:
-                    # Update value for another loop
-                    last_move = delta
-                    fake_path.append(target_cell)
-                # Check whether target_cell is connected to whatever path
-                connected_to_any_path  = MazeGenerateor.has_overlap(self.path, target_cell) or MazeGenerateor.has_overlap(self.fake_path, target_cell)
+                if self.check_goal(target_cell):
+                    # Fake path cannot break outside
+                    continue
+                goal = target_cell not in self.empty_cell  # if it walks into a non-empty cell = connected to existing path
+                if goal:
+                    path.append(target_cell)
+                    break
+                # Update value for another loop
+                last_move = delta
+                path.append(target_cell)
+                can_move = True
         
-        if not fake_goal:
-            for cells in fake_path:
-                self.matrix[cells[1]][cells[0]] = 1
-            self.fake_path += deepcopy(fake_path)
+        # Change value of horizontal and vertical walls
+        self.change_walls(path)
+        self.fake_paths.append(deepcopy(path))
+        return True
     
-    def generate(self, size):
+    def output(self, outside_thickness=5, name="maze"):
+        ''' Visualize the maze and save as png '''
+        
+        new_im = Image.new('RGB', (self.size_x*Cell_pixel+outside_thickness*2, self.size_y*Cell_pixel+outside_thickness*2), (255, 255, 255))
+
+        pixels = new_im.load() # create the pixel map
+
+        width = new_im.size[0]
+        height = new_im.size[1]
+
+        # Make outside walls
+        for x in range(outside_thickness):
+            for y in range(height):
+                pixels[x,y] = (0, 0 ,0)
+        for x in range(width-outside_thickness, width):
+            for y in range(height):
+                pixels[x,y] = (0, 0 ,0)
+
+        for x in range(width):
+            for y in range(outside_thickness):
+                pixels[x,y] = (0, 0 ,0)
+        for x in range(width):
+            for y in range(height-outside_thickness, height):
+                pixels[x,y] = (0, 0 ,0)
+
+        # Make inside walls
+        for x in range(outside_thickness, width, Cell_pixel):
+            for y in range(height):
+                pixels[x-1,y] = (0, 0 ,0)
+                pixels[x,y] = (0, 0 ,0)
+                pixels[x+1,y] = (0, 0 ,0)
+                
+        for x in range(width):
+            for y in range(outside_thickness, height, Cell_pixel):
+                pixels[x,y-1] = (0, 0 ,0)
+                pixels[x,y] = (0, 0 ,0)
+                pixels[x,y+1] = (0, 0 ,0)
+
+                
+        # Remove walls if it's not a wall
+        for y, row in enumerate(self.horizontal_walls):
+            for x, wall in enumerate(row):
+                if wall == 1:  # no wall
+                    if y == 0:  # break top wall
+                        for pixel_x in range(Cell_pixel-3):
+                            for pixel_y in range(outside_thickness+2):
+                                pixels[2+x*Cell_pixel + pixel_x + outside_thickness, pixel_y] = (255, 255, 255)
+                    elif y == self.size_y:  # break bottom wall
+                        for pixel_x in range(Cell_pixel-3):
+                            for pixel_y in range(outside_thickness+2):
+                                pixels[2+x*Cell_pixel + pixel_x + outside_thickness, height-pixel_y-1] = (255, 255, 255)
+                    else:  # inside walls
+                        for pixel_x in range(Cell_pixel-3):
+                            for pixel_y in range(3):
+                                pixels[2+x*Cell_pixel + pixel_x + outside_thickness, y*Cell_pixel + pixel_y + outside_thickness-1] = (255, 255, 255)
+
+        for y, row in enumerate(self.vertical_walls):
+            for x, wall in enumerate(row):
+                if wall == 1:  # no wall
+                    if x == 0:  # break left wall
+                        for pixel_y in range(Cell_pixel-3):
+                            for pixel_x in range(outside_thickness+2):
+                                pixels[pixel_x, 2+y*Cell_pixel + pixel_y + outside_thickness] = (255, 255, 255)
+                    elif x == self.size_x:  # break right wall
+                        for pixel_y in range(Cell_pixel-3):
+                            for pixel_x in range(outside_thickness+2):
+                                pixels[width-pixel_x-1, 2+y*Cell_pixel + pixel_y + outside_thickness] = (255, 255, 255)
+                    else:  # inside walls
+                        for pixel_x in range(3):
+                            for pixel_y in range(Cell_pixel-3):
+                                pixels[x*Cell_pixel + pixel_x + outside_thickness-1, 2+y*Cell_pixel + pixel_y + outside_thickness] = (255, 255, 255)
+
+        new_im.save(os.path.join('output', f'{name}.png'))
+
+    def generate(self, size_x:int, size_y:int, path_length=4):
         # Run self.generate(size) to generate a maze
+        # size_x: horizontal number of cells, size_y: vertical number of cells
 
+        self.path_length = path_length  # lenght of the correct path, ratio to size : 2 to 5
         self.path = []  # Valid path from start to finish
-        self.fake_path = []  # Fake paths
-        self.matrix = [[0 for x in range(size)] for x in range(size)]
-        self.size = size
-        generated_path = False
-        while not generated_path:
-            generated_path = self.create_path()
-        route_percent = sum(sum(self.matrix, [])) / self.size ** 2
-        # Generate fake paths until a portion of the maze is filled
-        while route_percent < self.fake_path_complexity:
-            self.generate_fake_path()
-            route_percent = sum(sum(self.matrix, [])) / self.size ** 2
+        self.fake_paths = []  # list of list, stores fake path, no practical use just yet
+        self.horizontal_walls = [[0 for x in range(size_x+1)] for y in range(size_y+1)]  # 0: wall, 1: through
+        self.vertical_walls = [[0 for x in range(size_x+1)] for y in range(size_y+1)]  # 0: wall, 1: through
+        self.empty_cell = set([(x, y) for x in range(size_x) for y in range(size_y)])
+        # 0 is black (wall), 1 is white (route)
+        self.size_x = size_x
+        self.size_y = size_y
+        success = self.create_path()
+        while not success:
+            success = self.create_path()
+        self.output(name="maze_solution")
 
-    def output(self, show_path=False):
-        ''' Visualize the maze and save as png 
-        show_path: False means don't indicate the correct path, True means indicating the correct path'''
+        while not self.empty_cell == set():
+            self.create_fake_path()
+        self.output()
 
-        matrix = deepcopy(self.matrix)
-        if not show_path:
-            start = self.path[0]
-            finish = self.path[-1]
-            matrix[start[1]][start[0]] = 2
-            matrix[finish[1]][finish[0]] = 2
-        else:
-            for solution in self.path:
-                matrix[solution[1]][solution[0]] = 2
 
-        # Create a new image, 
-        new_im = Image.new('RGBA', (Pixel*self.size, Pixel*self.size))
-        # Paste image by looping each cell
-        y_offset = 0
-        for r, row in enumerate(matrix):
-            x_offset = 0
-            for c, cell in enumerate(row):
-                new_im.paste(self.images[cell], (y_offset, x_offset))  # Paste the block of the maze
-                # Cover corner to make it looks better
-                if cell > 0:
-                    if r > 0 and r < self.size-1 and c > 0 and c < self.size-1:
-                        # Check top left
-                        if matrix[r][c-1] == 0 and matrix[r-1][c] == 0:
-                            new_im.paste(self.corner_images[0], (y_offset, x_offset), self.corner_images[0])
-                        # Check bottom left
-                        if matrix[r][c+1] == 0 and matrix[r-1][c] == 0:
-                            new_im.paste(self.corner_images[1], (y_offset, x_offset), self.corner_images[1])
-                        # Check bottom right
-                        if matrix[r][c+1] == 0 and matrix[r+1][c] == 0:
-                            new_im.paste(self.corner_images[2], (y_offset, x_offset), self.corner_images[2])
-                        # Check top right
-                        if matrix[r][c-1] == 0 and matrix[r+1][c] == 0:
-                            new_im.paste(self.corner_images[3], (y_offset, x_offset), self.corner_images[3])
-                x_offset += Pixel
-            y_offset += Pixel
-
-        filename = 'maze.png' if not show_path else 'maze_solution.png'
-        new_im.save(os.path.join('output', filename))
+if __name__ == '__main__':
+    MazeGenerateor().generate(30, 20)
